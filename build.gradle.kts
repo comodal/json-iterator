ae774f98e3d07f8eea22d518c5bcbf805c9e814f
James Edwards <james.p.edwards42@gmail.com>
2019-09-13 15:40:53 -0700

    Drop travis ci, bump gradle plugins.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
   `maven-publish`
   id("com.jfrog.bintray") version "1.8.4" apply false
   id("pl.allegro.tech.build.axion-release") version "1.10.2" apply false
-  id("org.javamodularity.moduleplugin") version "1.5.0" apply false
+  id("org.javamodularity.moduleplugin") version "1.6.0" apply false
   id("me.champeau.gradle.jmh") version "0.5.0-rc-2" apply false
   id("io.morethan.jmhreport") version "0.9.0" apply false
 }
